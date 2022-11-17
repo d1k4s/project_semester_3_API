9fd2c6f474f9386f64cd506b6f6c6bcf5fb59bd2
sulthon <sulthon.abady28@gmail.com>
2022-11-17 20:53:34 +0700

    fitur create user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
+const Validator = require('fastest-validator');
 var router = express.Router();
+
+const { User } = require('../models');
+
+const v = new Validator();
+const userController = require('../controllers/userController');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/', async (req, res) => {
+  const schema = {
+    firstName: 'string',
+    lastName: 'string',
+    email: 'string'
+  };
+
+  const validated = v.validate(req.body, schema);
+
+  if(validated.length) {
+    return res
+      .status(400)
+      .json(validated);
+  }
+
+  const user = await User.create(req.body);
+
+  res.json(user);
+})
 
 module.exports = router;
